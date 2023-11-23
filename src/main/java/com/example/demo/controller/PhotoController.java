f3a26969ca4a2bb4f2c12c7c6c77bad2a1bfeb70
MirkoGLostia <giggi.torre@mail.com>
2023-11-23 13:00:54 +0100

    update with fragments

--- src/main/java/com/example/demo/controller/PhotoController.java
+++ src/main/java/com/example/demo/controller/PhotoController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.exceptions.PhotoNotFoundException;
+import com.example.demo.model.Photo;
 import com.example.demo.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,7 +22,20 @@ public class PhotoController {
 
     @GetMapping
     public String index(@RequestParam Optional<String> search, Model model) {
+        model.addAttribute("area", "photo-list");
         model.addAttribute("photoList", photoService.getPhotoList(search));
         return "photos/list";
+    }
+
+    @GetMapping("detail/{id}")
+    public String detail(@PathVariable Integer id, Model model){
+        model.addAttribute("area", "photo-detail");
+        try {
+            Photo photo = photoService.getPhotoById(id);
+            model.addAttribute("photo", photo);
+            return "photos/detail";
+        } catch (PhotoNotFoundException e) {
+            throw new PhotoNotFoundException(e.getMessage());
+        }
     }
 }
--- src/main/java/com/example/demo/service/PhotoService.java
+++ src/main/java/com/example/demo/service/PhotoService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.exceptions.PhotoNotFoundException;
 import com.example.demo.model.Photo;
 import com.example.demo.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,15 @@ public class PhotoService {
             return photoRepository.findByTitleContainingIgnoreCase(search.get());
         } else {
             return photoRepository.findAll();
+        }
+    }
+
+    public Photo getPhotoById(Integer id) throws PhotoNotFoundException {
+        Optional<Photo> result = photoRepository.findById(id);
+        if (result.isPresent()) {
+            return result.get();
+        } else {
+            throw new PhotoNotFoundException("Photo with id " + id + " not found");
         }
     }
 }
