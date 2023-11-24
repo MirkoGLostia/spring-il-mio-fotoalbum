e7541e43b2169e4a377acefe0fb6b922e5e7aba3
MirkoGLostia <giggi.torre@mail.com>
2023-11-24 09:43:26 +0100

    visibility in edit-create

--- src/main/java/com/example/demo/service/PhotoService.java
+++ src/main/java/com/example/demo/service/PhotoService.java
@@ -42,6 +42,7 @@ public class PhotoService {
         photoToEdit.setTitle(photo.getTitle());
         photoToEdit.setDescription(photo.getDescription());
         photoToEdit.setImage(photo.getImage());
+        photoToEdit.setVisible(photo.getVisible());
         photoToEdit.setCategories(photo.getCategories());
 
         return photoRepository.save(photoToEdit);
