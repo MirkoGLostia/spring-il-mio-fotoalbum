dfd08b61c1688423e52b16266e90e4b59aadf594
MirkoGLostia <giggi.torre@mail.com>
2023-11-24 16:35:26 +0100

    api post message

---
+++ src/main/java/com/example/demo/api/MessageRestController.java
@@ -0,0 +1,25 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Message;
+import com.example.demo.model.User;
+import com.example.demo.service.MessageService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/message")
+public class MessageRestController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("/create")
+    public Message createMessage(@Valid @RequestBody Message mex) {
+        return messageService.saveMessage(mex);
+    }
+
+}
---
+++ src/main/java/com/example/demo/service/MessageService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Message;
+import com.example.demo.model.User;
+import com.example.demo.repository.MessageRepository;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public Message saveMessage(Message mex) {
+        mex.setId(null);
+        mex.setReadMex(true);
+        return messageRepository.save(mex);
+    }
+
+}
